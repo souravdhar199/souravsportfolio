2427e8f5b558fabbe8ff557030280b78ab14e613
sourav199 <souravdhar1999@gmail.com>
2022-09-23 12:07:45 -0400

    Created Navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import "../styles/navbar.css";
+function Navbar() {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>ABOUT</li>
+        <li>EXPERINCE</li>
+        <li>SKILLS</li>
+        <li>RESUME</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
