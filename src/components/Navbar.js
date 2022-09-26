7a0c48fd13844aee52dcb5854bdc1bfd50b83998
sourav199 <souravdhar1999@gmail.com>
2022-09-26 19:56:49 -0400

    SPELLING MISTAKE

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -16,7 +16,7 @@ function Navbar() {
         </li>
         <li>
           <Link activeClass="active" to="experience">
-            EXPERIRNCE
+            EXPERIENCE
           </Link>
         </li>
         <li>
