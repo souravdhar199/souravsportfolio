73f74f9c99758b509a60bfb33eed4778fbbf8a94
sourav199 <souravdhar1999@gmail.com>
2023-01-11 12:56:24 -0500

    Created typeWriter affect

--- src/components/About.js
+++ src/components/About.js
@@ -1,18 +1,31 @@
 import { IoIosSchool } from "react-icons/io";
 import img1 from "../images/profile.jpg";
 import "../styles/about.css";
+import Typewriter from "typewriter-effect";
 function About() {
   return (
     <section className="about">
       <div className="info">
         <h1>
-          Hi my name is <span>Sourav</span>{" "}
+          Hi my name is <span class="nameColor">Sourav</span>{" "}
         </h1>
         <h2 className="makeInline">
           <IoIosSchool className="icon2" />
           Georgia State University
         </h2>
-        <p>Upcoming CS grad, Looking for Entry level SWE role</p>
+        <Typewriter
+          onInit={(typewriter) => {
+            typewriter
+
+              .typeString("Looking for Entry Level Software Engineer Role")
+
+              .start();
+          }}
+          options={{
+            delay: 40,
+            wrapperClassName: "typed-wrapper",
+          }}
+        />
       </div>
       <div className="image">
         <img alt="profile" src={img1} />
